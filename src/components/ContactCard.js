9d7b4864d26d052eeff7a4104b48105b98f3371b
durga gautam <drggtm07@gmail.com>
2022-02-02 21:07:37 +0545

    initial commit

---
+++ src/components/ContactCard.js
@@ -0,0 +1,19 @@
+// import React from "react";
+
+// function ContactCard = () => {
+//     return(
+//         <div className="item">
+//         <div className="content">
+//             <div className="header">{contact.name}</div>
+//             <div>{contact.phone}</div>
+//         </div>
+//         <i  className="trash alternate outline icon"
+//         style={{colour:"red" marginTop:7px}}></i>
+//     </div>
+
+
+//     );
+
+// };
+
+// export default ContactCard;
